0a59f598d642ab3a8c25315c2508911b9c570575
PetrTolstov <57655896+PetrTolstov@users.noreply.github.com>
2020-10-11 22:43:26 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name = "description" content = "Site about films" > <!-- мета элемент, который описывает содержимое документа -->
+    <meta name = "keywords" content = "film, watch"> <!-- мета элемент, который описывает ключевые слова в документе -->
+    <meta name = "author" content = "Petr Tolstov"> <!-- мета элемент, который указывает имя автора документа -->
+    <meta charset="UTF-8" http-equiv="refresh" content="9999" >
+    <!--<meta  http-equiv  = "content-security-policy" content = "default-src https:" > мета элемент, который разрешает загрузку ресурсов (изображения, шрифты, скрипты и так далее) только с использованием https -->
+    <meta name = "robots" content = "index, follow"> <!-- Используется поисковыми системами при индексации страниц и прохода по ссылкам поисковой системой-->
+    <meta name = "viewport" content = "width=device-width, initial-scale = 1"> <!--Для того, чтобы мобильные браузеры автоматически не изменяли размер страниц сайта-->
+    <meta http-equiv = "X-UA-Compatible" content = "IE=edge">
+    <title>Base of kino</title>
+    <link rel="icon" href="style/icon.png" sizes="16x16 32х32" type="image/png">
+    <link href="style/style.css" type="text/css" rel="stylesheet">
+    <script src="scripts/page_script.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+</head>
+<body>
+<header class="photoHeader">
+    <hr>
+    <img src="src/ur_an.png">
+    <nav class="main_nav">
+        <ul>
+            <li><a href="">Главная</a></li>
+            <li><a href="">Случайное</a></li>
+            <li><a href="">Каталог</a></li>
+            <li><a href="">Топ аниме</a></li>
+            <li><a href="">Рейтинг пользователь</a></li>
+            <li><a href="">Помощь</a></li>
+
+        </ul>
+    </nav>
+</header>
+
+<main></main>
+</body>
+</html>
