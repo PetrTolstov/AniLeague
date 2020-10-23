fe263b776eee89f762a303ebedc6edad4380e59d
PetrTolstov <57655896+PetrTolstov@users.noreply.github.com>
2020-10-23 09:03:32 +0300

    23.10

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-const http = require('http');
-const hostname = '127.0.0.1';
-const port = 3000;
-const chalk = require('chalk')
+// const http = require('http');
+// const hostname = '127.0.0.1';
+// const port = 3000;
+// const chalk = require('chalk')
+//
+// const server = http.createServer( (req, res) => {
+//    res.statusCode = 200;
+//    res.setHeader('Content-Type', 'text/plain');
+//    res.end('Hello World\n');
+// });
+//
+// server.listen(port, hostname, () => {
+//     console.log(chalk.bold(chalk.green(`Server is running at http://${hostname}:${port}/`)));
+//
+// })
 
-const server = http.createServer( (req, res) => {
-   res.statusCode = 200;
-   res.setHeader('Content-Type', 'text/plain');
-   res.end('Hello World\n');
-});
+const os = require('os')
 
-server.listen(port, hostname, () => {
-    console.log(chalk.bold(chalk.green(`Server is running at http://${hostname}:${port}/`)));
+console.log(os.freemem())
 
-})
+console.log(os.totalmem())
